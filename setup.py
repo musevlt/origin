d5cc1b2e371d3cf44d5b4b9b48f9991ebb730300
piqueras <laure.piqueras@univ-lyon1.fr>
2016-10-13 11:20:02 +0200

    installation must copy the file which contains lines profiles

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='origin',
     version='1.0',
     packages=find_packages(),
-    package_data={'origin': ['mumdatMask_1x1/*.fits.gz']},
+    package_data={'origin': ['Dico_FWHM_2_12.mat']},
     zip_safe=False,
     install_requires=['numpy', 'scipy', 'matplotlib', 'astropy', 'mpdaf', 'six'],
     tests_require=['pytest'],
