3a3a251e42ea6aadabafbba95c072bef0830cb08
piqueras <laure.piqueras@univ-lyon1.fr>
2016-11-23 15:30:34 +0100

    final catalog with the working format

--- origin/origin.py
+++ origin/origin.py
@@ -1090,7 +1090,7 @@ class ORIGIN(object):
                                               
         # create the final catalog
         self._log_stdout.info('Create the final catalog')
-        catF = Catalog.from_path(path_src)
+        catF = Catalog.from_path(path_src, fmt='working')
         catF.write(catname)
                       
         self._log_file.info('10 Done')
