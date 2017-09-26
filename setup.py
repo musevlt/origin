9dd9e465d5383e21e0fe193f8d3c012ad5fb0c20
piqueras <laure.piqueras@univ-lyon1.fr>
2017-09-26 15:57:32 +0200

    version updated in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='origin',
-    version='1.0',
+    version='2.0beta',
     packages=find_packages(),
     package_data={'origin': ['Dico_FWHM_2_12.fits']},
     zip_safe=False,
