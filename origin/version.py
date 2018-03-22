b1e4e6b72352455f93149f6cd2ad8109379ee4f8
Simon Conseil <simon.conseil@univ-lyon1.fr>
2018-03-22 15:49:18 +0100

    Fix dev version when outside of origin directory

--- origin/version.py
+++ origin/version.py
@@ -1,17 +1,17 @@
+import os
+
 __version__ = '3.0.dev'
 # __description__ = ''
 
+CURDIR = os.path.dirname(os.path.abspath(__file__))
+
 
 def _update_git_version():
-    import subprocess
-    command_number = 'git rev-list --count HEAD'
-    try:
-        commit_number = subprocess.check_output(command_number, shell=True)\
-            .decode('ascii').strip()
-    except Exception:
-        pass
-    else:
-        return commit_number
+    from contextlib import suppress
+    from subprocess import check_output
+    command_number = 'git -C {} rev-list --count HEAD'.format(CURDIR).split()
+    with suppress(Exception):
+        return check_output(command_number).decode('ascii').strip()
 
 
 try:
