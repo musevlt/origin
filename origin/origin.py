f0e9129a1349847e76a5465e8c33213089c839e9
Simon Conseil <simon.conseil@univ-lyon1.fr>
2019-04-05 11:59:08 +0200

    Fix yaml loading again, use unsafe_load

--- origin/origin.py
+++ origin/origin.py
@@ -37,10 +37,10 @@ from .lib_origin import timeit
 from .version import __version__
 
 try:
-    # With PyYaml 5.1, load and safe have been renamed to full_* and
+    # With PyYaml 5.1, load and safe have been renamed to unsafe_* and
     # replaced by the safe_* functions. We need the full ones to
     # be able to dump Python objects, yay!
-    from yaml import full_load as load_yaml, dump as dump_yaml
+    from yaml import unsafe_load as load_yaml, dump as dump_yaml
 except ImportError:
     from yaml import load as load_yaml, dump as dump_yaml
 
