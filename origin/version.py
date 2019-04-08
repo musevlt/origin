bd27ad91c6b754f4c528fc997f4ffb8bf5e655af
Simon Conseil <simon.conseil@univ-lyon1.fr>
2019-04-08 15:37:32 +0200

    Switch to setuptools_scm

--- origin/version.py
+++ origin/version.py
@@ -1,26 +1,6 @@
-import os
-
-__version__ = '3.1.dev'
-# __description__ = ''
-
-CURDIR = os.path.dirname(os.path.abspath(__file__))
-
-
-def _update_git_version():
-    from contextlib import suppress
-    from subprocess import check_output
-    command_number = 'git -C {} rev-list --count HEAD'.format(CURDIR).split()
-    with suppress(Exception):
-        return check_output(command_number).decode('ascii').strip()
-
-
+from pkg_resources import get_distribution, DistributionNotFound
 try:
-    if '.dev' in __version__:
-        commit_number = _update_git_version()
-        if commit_number:
-            __version__ += commit_number
-        else:
-            from ._githash import __dev_value__
-            __version__ += __dev_value__
-except Exception:
-    pass
+    __version__ = get_distribution('origin').version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = None
--- setup.py
+++ setup.py
@@ -1,44 +1,13 @@
-import os
 import sys
 from setuptools import setup, find_packages
-from subprocess import check_output
 
 if sys.version_info[:2] < (3, 5):
     sys.exit('Origin supports Python 3.5+ only')
 
-# Read version.py
-__version__ = None
-__description__ = None
-with open('origin/version.py') as f:
-    exec(f.read())
-
-# If the version is not stable, we can add a git hash to the __version__
-if '.dev' in __version__:
-    # Find hash for __githash__ and dev number for __version__ (can't use hash
-    # as per PEP440)
-    command_hash = 'git rev-list --max-count=1 --abbrev-commit HEAD'
-    command_number = 'git rev-list --count HEAD'
-
-    try:
-        commit_hash = check_output(command_hash, shell=True)\
-            .decode('ascii').strip()
-        commit_number = check_output(command_number, shell=True)\
-            .decode('ascii').strip()
-    except Exception:
-        pass
-    else:
-        # We write the git hash and value so that they gets frozen if installed
-        with open(os.path.join('origin', '_githash.py'), 'w') as f:
-            f.write("__githash__ = \"{}\"\n".format(commit_hash))
-            f.write("__dev_value__ = \"{}\"\n".format(commit_number))
-
-        # We modify __version__ here too for commands such as egg_info
-        # __version__ += commit_number
-
 setup(
     name='origin',
-    version=__version__,
-    description=__description__,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     packages=find_packages(),
     include_package_data=True,
     package_data={'origin': ['Dico_FWHM_2_12.fits']},
